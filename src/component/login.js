fcaeff97fd8f22103dbf9789c67da43e296e94eb
שרה טורצין <211737739@mbydomain>
2021-05-19 19:08:33 +0300

    hazata-meucheret

--- src/component/login.js
+++ src/component/login.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
-import { Button, Input, TextField } from '@material-ui/core';
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import React, { useState ,useEffect} from 'react';
+import { Button, TextField } from '@material-ui/core';
 import './login.css'
 import { useHistory } from "react-router-dom";
-import { useForm } from "react-hook-form";
 function Login() {
-    const  register= useForm();
-    const [email, setEmail] = useState(''); 
-    const [password, setPassword] = useState(''); 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [hasErrror, setHasErrror] = useState(true);
+
+    useEffect(() => {
+        if (email !== "" & password !== "" )
+            setHasErrror(false)
+        else
+            setHasErrror(true)
+    }, [email, password]);
+
 
     const history = useHistory();
     const home = () => {
@@ -18,39 +24,28 @@ function Login() {
     const signup = () => {
         history.push('/signup');
     }
-    
+
+
+
     return (
         <div>
-           <h1>wellcom</h1>
-           <h3>we happy you return to ower sekerGraph </h3>
-            <ValidatorForm >
-             {/* onChange={(e) => check(e.target.value)} > */}
-                <TextValidator
-                    label="enter your email:"
-                    onChange={(e) => setEmail(e.target.value)}
-                    name="email"    
-                    value={email}
-                    validators={['required', 'isEmail']}
-                    errorMessages={['this field is required','email is not valid']}
-                />
-                {/* </ValidatorForm> */}
+            <h1>wellcom</h1>
+            <h3>we happy you return to owur sekerGraph </h3>
+            <form noValidate autoComplete="off"><div> <TextField id="standard-basic" label="enter your Email*"
+                onChange={(e) => {
+                    setEmail(e.target.value)
+                }} /></div>
+                <div><TextField id="standard-basic"  type="password" label="enter your password*"
+                    onChange={(e) => {
+                        setPassword(e.target.value)
+                    }} /></div>
 
-            {/* <ValidatorForm onChange={(e) => setPassword(e.target.value)} > */}
-                <TextValidator
-                    label="enter your password:"
-                    onChange={(e) => setPassword(e.target.value)}
-                    name="password"    
-                    type="password"
-                    validators={['required','minLength:8', 'maxLength:15']}
-                    errorMessages={['this field is required','must be between 8-15 chars','must be  8-15 chars']}
-                    value={password}
-                />
-                </ValidatorForm>
-            <Button type="submit" variant="contained" color="secondary" onClick={home}>ENTER</Button><br /><br />
-            <Button variant="contained" color="secondary" onClick={signup}>CONNECTION</Button>
-           
-               
-            
+
+                <Button type="submit" disabled={hasErrror} variant="contained" color="secondary" onClick={home}>ENTER</Button><br /><br />
+                <Button variant="contained" color="secondary" onClick={signup}>CONNECTION</Button>
+
+            </form>
+
         </div>
     )
 
--- src/component/signup.js
+++ src/component/signup.js
@@ -1,43 +1,124 @@
-import React, {useState}from 'react';
-import { Button, Input, TextField } from '@material-ui/core'
+import React, { useEffect, useState } from 'react';
+import { Button, TextField } from '@material-ui/core'
 import { useHistory } from "react-router-dom";
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Signup(){
+function Signup() {
 
-    const [name, setName] = useState(''); 
-    const [email, setEmail] = useState(''); 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [flagPassword, setFlagPassword] = useState(false);
+    const [flagEmail, setFlagEmail] = useState(false);
+    const [flagName, setFlagName] = useState(false);
+    const [emailMass, setEmailMass] = useState('');
+    const [passwordMass, setPasswordMass] = useState('');
+    const [nameMass, setNameMass] = useState('');
+    const [hasErrror, setHasErrror] = useState(true);
+
     const history = useHistory();
     const login = () => {
         history.push('/');
     }
-    return(
+    useEffect(() => {
+        if (email !== "" & name !== "" & password !== "" & !flagPassword & !flagEmail)
+            setHasErrror(false)
+        else
+            setHasErrror(true)
+    }, [email, name, password]);
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            '& .MuiTextField-root': {
+                margin: theme.spacing(1),
+                width: 200,
+            },
+        },
+    }));
+    const classes = useStyles();
+
+    function isEmail(val) {
+        let regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!regEmail.test(val))
+            return true;
+        return false;
+    }
+
+
+    return (
         <div>
-             <h1>wellcom</h1>
-           <h3>we happy you choose to use ower sekerGraph </h3>
-            <p>enter your name:</p>
-            <ValidatorForm onChange={(e) => setName(e.target.value)} >
-                <TextValidator
-                    label="Name"
-                    onChange={(e) => setName(e.target.value)}
-                    name="name"    
-                    value={name}
-                    // validators={['required', 'isEmail']}
-                    errorMessages={['this field is required']}
-                /></ValidatorForm>
-            <p>enter your email:</p>
-            <ValidatorForm onChange={(e) => setEmail(e.target.value)} >
-                <TextValidator
-                    label="Email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    name="email"    
-                    value={email}
-                    validators={['required', 'isEmail']}
-                    errorMessages={['this field is required','email is not valid']}
-                /></ValidatorForm>
-            <p>enter your password:</p>
-            <input /><br/><br/>
-            <Button variant="contained" color="secondary" onClick={login}>Registration</Button>
+            <h1>wellcom</h1>
+            <h3>we happy you choose to use ower sekerGraph </h3>
+
+
+            <form className={classes.root} noValidate autoComplete="off">
+
+                <div>
+                    <TextField
+                        error={flagEmail}
+                        id="standard-error-helper-text"
+                        label="enter your Email*"
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                            if (!flagEmail)
+                                setEmailMass("")
+                        }}
+                        onBlur={() => {
+                            setFlagEmail(isEmail(email))
+                            if (flagEmail)
+                                setEmailMass("email is not valid")
+                        }}
+                        helperText={emailMass}
+                    />
+                </div>
+
+                <div>
+                    <TextField
+                        error={flagPassword}
+                        id="standard-error-helper-text"
+                        label="enter your password*"
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                            if (password.length + 1 >= 8) {
+                                setPasswordMass("");
+                                setFlagPassword(false);
+                            }
+                        }}
+                        type="password"
+                        helperText={passwordMass}
+                        onBlur={() => {
+                            if (password.length < 8) {
+                                setPasswordMass("must be at least 8 chars");
+                                setFlagPassword(true);
+                            }
+                        }}
+                    />
+                </div>
+
+                <div>
+                    <TextField
+                        error={flagName}
+                        id="standard-error-helper-text"
+                        label="enter your name*"
+                        onChange={(e) => {
+                            setName(e.target.value);
+                            if (name !== "") {
+                                setNameMass("");
+                                setFlagName(false);
+                            }
+                        }}
+                        helperText={nameMass}
+                        onBlur={() => {
+                            if (name === "") {
+                                setNameMass("must be full");
+                                setFlagName(true)
+                            }
+                        }}
+
+                    />
+                </div>
+                <Button disabled={hasErrror} variant="contained" color="secondary" onClick={login}>Registration</Button>
+            </form>
         </div>
     )
 }
